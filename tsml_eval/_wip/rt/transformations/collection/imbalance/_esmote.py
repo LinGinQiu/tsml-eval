9de6c497005db7ac63bab987b563dc0b9cacdfd9
Chris Qiu <chuanhangqiu@outlook.com>
2025-07-24 15:47:16 +0100

    esmote with smooth

--- tsml_eval/_wip/rt/transformations/collection/imbalance/_esmote.py
+++ tsml_eval/_wip/rt/transformations/collection/imbalance/_esmote.py
@@ -110,7 +110,7 @@ class ESMOTE(BaseCollectionTransformer):
         for class_sample, n_samples in self.sampling_strategy_.items():
             if n_samples == 0:
                 continue
-            n_samples = n_samples  # increase the number of samples to process selection
+            n_samples = 2*n_samples  # increase the number of samples to process selection
             target_class_indices = np.flatnonzero(y == class_sample)
             X_class = X[target_class_indices]
             y_class = y[target_class_indices]
@@ -131,7 +131,7 @@ class ESMOTE(BaseCollectionTransformer):
             y_resampled.append(y_new)
         X_synthetic = np.vstack(X_resampled)
         y_synthetic = np.hstack(y_resampled)
-        if False:
+        if True:
             from warnings import warn
             from tsml_eval._wip.rt.transformations.collection.imbalance._utils import SyntheticSampleSelector
             selector = SyntheticSampleSelector(random_state=self.random_state)
