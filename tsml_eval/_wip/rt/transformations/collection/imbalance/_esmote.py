02950ec0f8b7707dd0e42068c1cc4aafde5f0f26
Chris Qiu <chuanhangqiu@outlook.com>
2025-09-19 15:27:19 +0800

    esmote3

--- tsml_eval/_wip/rt/transformations/collection/imbalance/_esmote.py
+++ tsml_eval/_wip/rt/transformations/collection/imbalance/_esmote.py
@@ -218,38 +218,38 @@ class ESMOTE(BaseCollectionTransformer):
                 X_iter = X_class.copy()
                 y_iter = y_class.copy()
                 gen_num = 0
-                distance_funcs = ['adtw', 'twe']
-                n_samples_slice = int(n_samples / (2 * len(distance_funcs)))
-                for _ in tqdm.tqdm(range(3)):
-                    X_new_slice = []
-                    y_new_slice = []
-                    for distance_func in distance_funcs:
-                        self.ditance = distance_func
-                        nn_temp_ = KNeighborsTimeSeriesClassifier(
+                distance_funcs = ['adtw', 'twe', 'msm']
+                n_samples_slice = int(n_samples / (len(distance_funcs)))
+                for _ in tqdm.tqdm(range(5)):
+                    distance_fuc = self._random_state.choice(distance_funcs)
+                    self.distance = distance_fuc
+                    # X_new_slice = []
+                    # y_new_slice = []
+                    nn_temp_ = KNeighborsTimeSeriesClassifier(
                             n_neighbors=self.suggested_n_neighbors_ + 1,
                             distance=self.distance,
                             distance_params=self._distance_params,
                             weights=self.weights,
                             n_jobs=self.n_jobs,
-                        )
+                    )
 
-                        nn_temp_.fit(X_iter, y_iter)
-                        nns = nn_temp_.kneighbors(X=X_iter, return_distance=False)[:, 1:]
+                    nn_temp_.fit(X_iter, y_iter)
+                    nns = nn_temp_.kneighbors(X=X_iter, return_distance=False)[:, 1:]
 
-                        X_new_slice_p, y_new_slice_p = self._make_samples(
-                            X_iter,
-                            y.dtype,
-                            class_sample,
-                            X_iter,
-                            nns,
-                            n_samples_slice,
-                            1.0,
-                            n_jobs=self.n_jobs,
-                        )
-                        X_new_slice.append(X_new_slice_p)
-                        y_new_slice.append(y_new_slice_p)
-                    X_new_slice = np.vstack(X_new_slice)
-                    y_new_slice = np.hstack(y_new_slice)
+                    X_new_slice_p, y_new_slice_p = self._make_samples(
+                        X_iter,
+                        y.dtype,
+                        class_sample,
+                        X_iter,
+                        nns,
+                        n_samples_slice,
+                        1.0,
+                        n_jobs=self.n_jobs,
+                    )
+                    # X_new_slice.append(X_new_slice_p)
+                    # y_new_slice.append(y_new_slice_p)
+                    X_new_slice = X_new_slice_p
+                    y_new_slice = y_new_slice_p
                     # select the samples that are classified as the minority class with high probability
                     prob = discriminator.predict_proba(X_new_slice)
                     class_indices = np.where(discriminator.classes_ == class_sample)[0][0]
@@ -263,8 +263,8 @@ class ESMOTE(BaseCollectionTransformer):
                     X_new.append(X_new_slice.copy())
                     y_new.append(y_new_slice.copy())
                     gen_num += len(X_new_slice)
-                    X_iter = X_new_slice
-                    y_iter = y_new_slice
+                    X_iter = np.concatenate((X_iter, X_new_slice))
+                    y_iter = np.concatenate((y_iter, y_new_slice))
                     if gen_num >= n_samples:
                         break
 
