a21fb0e08b4cffc17a85683a60eb2026ed0caba0
Chris Qiu <chuanhangqiu@outlook.com>
2026-02-08 14:06:45 +0000

    training time

--- tsml_eval/_wip/rt/transformations/collection/imbalance/LGD_VAE/src/nn/model.py
+++ tsml_eval/_wip/rt/transformations/collection/imbalance/LGD_VAE/src/nn/model.py
@@ -1055,9 +1055,11 @@ class LatentGatedDualVAE(nn.Module):
             if not turn_off_dilate:
                 from tsml_eval._wip.rt.transformations.collection.imbalance.LGD_VAE.loss.dilate_loss import dilate_loss
                 recon_loss_dilate, _, _ = dilate_loss(outputs=recon, targets=x, device=x.device, alpha=0.8)
+                recon_loss = recon_loss_dilate
             else:
                 recon_loss_dilate = torch.tensor(0.0, device=x.device)
-            recon_loss = recon_loss_dilate + recon_loss_mse
+                recon_loss = recon_loss_dilate + recon_loss_mse
+
         elif self.recon_metric == 'mse':
             recon_loss = recon_loss_mse
         else:
